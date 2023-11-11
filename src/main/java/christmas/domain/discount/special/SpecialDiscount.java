87813a816e8d10cea0ceaecc744291ef14ca7e67
uhanuu <hwryu990927@gmail.com>
2023-11-11 18:53:58 +0900

    feat: special discount로직

--- src/main/java/christmas/domain/discount/special/SpecialDiscount.java
+++ src/main/java/christmas/domain/discount/special/SpecialDiscount.java
@@ -1,5 +1,32 @@
 package christmas.domain.discount.special;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public enum SpecialDiscount {
-    EVENT_CALENDAR_STAR()
+    EVENT_CALENDAR_STAR(1000, 25);
+
+    private final int discountPrice;
+    private final int christmasDay;
+
+    SpecialDiscount(int discountPrice, int christmasDay) {
+        this.discountPrice = discountPrice;
+        this.christmasDay = christmasDay;
+    }
+
+    public static boolean isCalendarStarEvent(LocalDate localDate) {
+        return isSunday(localDate) || isChristmasDay(localDate);
+    }
+
+    private static boolean isSunday(LocalDate localDate) {
+        return localDate.getDayOfWeek() == DayOfWeek.SUNDAY;
+    }
+
+    private static boolean isChristmasDay(LocalDate localDate) {
+        return localDate.getDayOfMonth() == EVENT_CALENDAR_STAR.christmasDay;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
 }
--- src/main/java/christmas/domain/discount/special/SpecialDiscountPolicy.java
+++ src/main/java/christmas/domain/discount/special/SpecialDiscountPolicy.java
@@ -3,13 +3,21 @@ package christmas.domain.discount.special;
 import christmas.domain.discount.DiscountPolicy;
 import christmas.service.dto.DiscountDto;
 
+import java.time.LocalDate;
+
+import static christmas.domain.discount.special.SpecialDiscount.EVENT_CALENDAR_STAR;
+import static christmas.domain.discount.special.SpecialDiscount.isCalendarStarEvent;
+
 public class SpecialDiscountPolicy implements DiscountPolicy {
 
     private static final int DEFAULT_DISCOUNT = 0;
 
     @Override
     public int discount(DiscountDto discountDto) {
-
-        return 0;
+        LocalDate localDate = discountDto.getLocalDate();
+        if (isCalendarStarEvent(localDate)) {
+            return EVENT_CALENDAR_STAR.getDiscountPrice();
+        }
+        return DEFAULT_DISCOUNT;
     }
 }
