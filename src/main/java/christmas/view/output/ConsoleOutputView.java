cbe17c96f1fba8130062f6aeb36a3a38598bc917
uhanuu <hwryu990927@gmail.com>
2023-11-13 13:36:31 +0900

    refactor: output view 상수처리

--- src/main/java/christmas/view/output/ConsoleOutputView.java
+++ src/main/java/christmas/view/output/ConsoleOutputView.java
@@ -1,61 +1,56 @@
 package christmas.view.output;
 
 import christmas.controller.dto.OrderMenuDto;
-import christmas.domain.event.discount.dto.DiscountInfo;
 import christmas.domain.event.item.GiftItem;
 import christmas.service.dto.DiscountInfoWithGiftItemDTO;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import static christmas.view.output.PriceFormat.BENEFIT_PRICE_FORMAT;
+import static christmas.view.output.PriceFormat.ORDER_PRICE_FORMAT;
+
 public class ConsoleOutputView {
-    private static final String MENU_FORMAT = "%s %s개\n";
-    private static final NumberFormat ORDER_PRICE_FORMAT = new DecimalFormat("###,###원");
-    private static final NumberFormat BENEFIT_PRICE_FORMAT = new DecimalFormat("-###,###원");
 
     public void printStartMessage(LocalDate localDate) {
         int month = localDate.getMonth().getValue();
         int dayOfMonth = localDate.getDayOfMonth();
-        String messageFormat = "%s월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
-
-        System.out.println(String.format(messageFormat, month, dayOfMonth));
+        System.out.println(String.format(OutputMessage.START_MESSAGE_FORMAT.getMessage(), month, dayOfMonth));
         System.out.println();
     }
 
     public void printAllOrderMenu(List<OrderMenuDto> orderMenus) {
-        System.out.println("<주문 메뉴>");
+        System.out.println(OutputMessage.ORDER_MENU_MESSAGE.getMessage());
         orderMenus.forEach(this::printOrderMenu);
         System.out.println();
     }
     private void printOrderMenu(OrderMenuDto orderMenu) {
         String name = orderMenu.getName();
         int quantity = orderMenu.getQuantity();
-        System.out.printf(MENU_FORMAT, name, quantity);
+        System.out.printf(OutputMessage.MENU_FORMAT.getMessage(), name, quantity);
     }
 
     public void printTotalOrderPrice(int totalOrderPrice) {
-        System.out.println("<할인 전 총주문 금액>");
+        System.out.println(OutputMessage.TOTAL_ORDER_PRICE_MESSAGE.getMessage());
         System.out.println(ORDER_PRICE_FORMAT.format(totalOrderPrice));
         System.out.println();
     }
 
     public void printGiftMenu(Optional<GiftItem> giftItem) {
-        System.out.println("<증정 메뉴>");
+        System.out.println(OutputMessage.GIFT_ITEM_MESSAGE.getMessage());
         giftItem.ifPresentOrElse(
-                item -> System.out.printf(MENU_FORMAT, item.getName(), item.getQuantity()),
-                () -> System.out.print("없음")
+                item -> System.out.printf(OutputMessage.MENU_FORMAT.getMessage(), item.getName(), item.getQuantity()),
+                () -> System.out.print(OutputMessage.NO_ITEM_MESSAGE.getMessage())
         );
-        System.out.println("\n");
+        System.out.println();
     }
 
     public void printBenefitDetails(List<DiscountInfoWithGiftItemDTO> discountInfosWithGiftItem) {
-        System.out.println("<혜택 내역>");
+        System.out.println(OutputMessage.BENEFIT_DETAILS_MESSAGE.getMessage());
 
         if (discountInfosWithGiftItem.isEmpty()) {
-            System.out.println("없음");
+            System.out.println(OutputMessage.NO_ITEM_MESSAGE.getMessage());
         }
         discountInfosWithGiftItem
                 .forEach(info -> System.out.println(
@@ -65,19 +60,19 @@ public class ConsoleOutputView {
     }
 
     public void printTotalBenefitPrice(int totalBenefitPrice) {
-        System.out.println("<총혜택 금액>");
+        System.out.println(OutputMessage.TOTAL_BENEFIT_PRICE_MESSAGE.getMessage());
         System.out.println(BENEFIT_PRICE_FORMAT.format(totalBenefitPrice));
         System.out.println();
     }
 
     public void printEstimatedPayment(int estimatedPayment) {
-        System.out.println("<할인 후 예상 결제 금액>");
+        System.out.println(OutputMessage.ESTIMATED_PAYMENT_MESSAGE.getMessage());
         System.out.println(ORDER_PRICE_FORMAT.format(estimatedPayment));
         System.out.println();
     }
 
     public void printDecemberEventBadge(String badgeName) {
-        System.out.println("<12월 이벤트 배지>");
+        System.out.println(OutputMessage.DECEMBER_EVENT_BADGE_MESSAGE.getMessage());
         System.out.println(badgeName);
     }
 
---
+++ src/main/java/christmas/view/output/OutputMessage.java
@@ -0,0 +1,24 @@
+package christmas.view.output;
+
+public enum OutputMessage {
+    MENU_FORMAT("%s %s개\n"),
+    START_MESSAGE_FORMAT("%s월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+    ORDER_MENU_MESSAGE("<주문 메뉴>"),
+    TOTAL_ORDER_PRICE_MESSAGE("<할인 전 총주문 금액>"),
+    GIFT_ITEM_MESSAGE("<증정 메뉴>"),
+    NO_ITEM_MESSAGE("없음"),
+    BENEFIT_DETAILS_MESSAGE("<혜택 내역>"),
+    TOTAL_BENEFIT_PRICE_MESSAGE("<총혜택 금액>"),
+    ESTIMATED_PAYMENT_MESSAGE("<할인 후 예상 결제 금액>"),
+    DECEMBER_EVENT_BADGE_MESSAGE("<12월 이벤트 배지>");
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
